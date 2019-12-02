02e4f9b14a58d345fb84c78183176982fdb23102
kylehz <kylehz0617@gmail.com>
2019-12-02 11:10:58 +0800

    update listen addr

--- webapi/main.py
+++ webapi/main.py
@@ -16,4 +16,4 @@ def get_task(address):
     return jsonify(datas[address])
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=9000)
+    app.run(debug=True, host="0.0.0.0", port=9000)
